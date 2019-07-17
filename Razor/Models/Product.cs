b8a3b03cd58b3af5ec0d8a7c538d0777f6bdd811
Michael Zulfekar <n00929403@ospreys.unf.edu>
2019-07-16 23:44:44 -0400

    Creating the Controller

--- Razor/Models/Product.cs
+++ Razor/Models/Product.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Razor.Model
+﻿namespace Razor.Models
 {
+
     public class Product
     {
+
         public int ProductID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
--- Razor/Startup.cs
+++ Razor/Startup.cs
@@ -11,14 +11,13 @@ namespace Razor
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -27,8 +26,8 @@ namespace Razor
             }
 
             //app.Run(async (context) => {
-            //   await context.Response.WriteAsync("Hello World!");
-            // });
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
             app.UseMvcWithDefaultRoute();
         }
     }
