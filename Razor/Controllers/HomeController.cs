02def4d5e1e76fba33e7ecde8640b22c9d1aed1a
Michael Zulfekar <n00929403@ospreys.unf.edu>
2019-07-17 02:41:43 -0400

    Using an Array in the HomeController.cs File

--- Razor/Controllers/HomeController.cs
+++ Razor/Controllers/HomeController.cs
@@ -6,18 +6,13 @@ namespace Razor.Controllers
     {
         public ViewResult Index()
         {
-            Product myProduct = new Product
-            {
-                ProductID = 1,
-                Name = "Kayak",
-                Description = "A boat for one person",
-                Category = "Watersports",
-                Price = 275M
-            };
-
-            ViewBag.StockLevel = 2;
-
-            return View(myProduct);
+            Product[] array = {
+             new Product {Name = "Kayak", Price = 275M},
+             new Product {Name = "Lifejacket", Price = 48.95M},
+             new Product {Name = "Soccer ball", Price = 19.50M},
+             new Product {Name = "Corner flag", Price = 34.95M}
+             };
+            return View(array);
         }
     }
 }
